b78564f33e360740283508c6d1f368e6ebec1c75
amorfati0310 <lyhy0310@gmail.com>
2020-05-23 16:36:03 +0900

    refactor: 내가 생각한 데로 우선 리팩토링

--- 01/statementByDali.js
+++ 01/statementByDali.js
@@ -11,59 +11,80 @@ return result string
 // 1. return type 이나 param type 명시되면 좋을 듯  
 // 2. statement -> getStatement 
 // 함수는 동사 + 명사 행위나 의도를 나타내면 더 좋을 듯 
-// 3. invoice 에서 perofrmance만 필요하다
+// 3. destructuring 활용 해서 프로퍼티들을 좀 나눈다 
+// 4. for of -> 보다 Array instance methods가 가독성 밑 조작이 편의하다.
 
 
+// 5. thisAmout 구하는 switchCase를 분리하자 
 
 
+//6. 행위별로 함수를 분리한다. 
+// 6.1 토탈 어마운트 계산
+// 6.2 포인트 적립
+// 6.3 청구내역 출력
 // ETC `
 // format  new Intl -> 잘 모름 일단 넘어가자 
 
-function statment({performances}, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${inovice.customer})\n`
+
+
+function statment({performances, customer}, plays) {
+    const initStatement = `청구 내역 (고객명: ${customer})\n`
     const format = new Intl.NumberFormat("en-Us", {
         style: "currency",
         currency: "USD",
         minimumFractionDigits: 2
     }).format
 
-    for(let perf of performances) {
-        const play = plays[perf.playID]
-        let thisAmount = 0;
 
-        switch(play.type){
+    const getAmountByType = (type, audience) => {
+        switch(type){
             case "tragedy": {
-                thisAmount = 40000; 
-                if(perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30)
+                let amount = 40000; 
+                if(audience > 30) {
+                    amount += 1000 * (audience - 30)
                 }
-                break;
+                return amount;
             }
             case "comedy": {
-                thisAmount = 30000; 
-                if(perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20)
+                let amount = 30000;
+                if(audience > 20) {
+                    amount += 10000 + 500 * (audience - 20)
                 }
-                thisAmount += 300 * perf.audience
-                break;
+                return amount += 300 * audience
             }
             default: {
-                throw new Error(`알 수 없는 장르 ${play.type}`)
+                throw new Error(`알 수 없는 장르 ${type}`)
             }
         }
-        // 포인트를 적립한다 
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // 희극 관객 5명 마다 추가 포인트를 제공한다.
-        if('comedy' === play.type){
-            volumeCredits += Math.floor(perf.audience / 5)
-        }
-        // 청구 내역을 출력한다. 
-        result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience} 석) \n`
-        totalAmount += thisAmount
     }
-    result += `총액 ${format(totalAmount / 100)}`
-    return result
+
+ 
+
+
+    const toatalAmount = performances.reduce((acc, {playID, audience}) => {
+        const {type} = plays[playID]
+        const amount = getAmountByType(type, audience)
+        return amount + acc;
+    }, 0);
+
+
+    const volumeCredits = performances.reduce((acc, {audience, playID}) => {
+        const {type} = plays[playID]
+        let credits = acc + Math.max(audience - 30, 0)
+        if(type === 'comedy'){
+            credits += Math.floor(audience / 5)
+        }
+        return credits;
+    }, 0);
+
+    const printStatementsByPeformance = performances.reduce((acc, {audience, playID}) => {
+        const {type, name} = plays[playID]
+        result += `${name}: ${format(getAmountByType(type, audience) / 100)} (${audience} 석) \n`
+       
+    }, ``)
+    return `
+        ${initStatement}
+        ${printStatementsByPeformance}
+        총액 ${format(totalAmount / 100)}
+    `
 }
