7fcb390aa8b74ca27b024b6c819a003365bfb134
amorfati0310 <lyhy0310@gmail.com>
2020-05-23 16:54:36 +0900

    refactor switch case -> amountFor fn

--- 01/statementByBook.js
+++ 01/statementByBook.js
@@ -1,3 +1,13 @@
+// checkList 
+
+// 별도의 함수로 빼냈을 때 값을 변경시키는지 ? 
+// 매개변수로서만 참조만 한다면 괜찮음 
+
+// 하나 고친 후 테스트 코드를 돌려서 확인한다. 
+// -> 변경점이 많은 후에 테스트 하면 -> 디버깅이 어렵기 때문이다 
+
+// 조금씩 수정하여 피드백 주기를 짧게 가져간다. 
+
 function statment(invocie, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -8,30 +18,35 @@ function statment(invocie, plays) {
         minimumFractionDigits: 2
     }).format
 
-    for(let perf of invocie.performances) {
-        const play = plays[perf.playID]
+    function amountFor(perf, play){
         let thisAmount = 0;
-
         switch(play.type){
             case "tragedy": {
                 thisAmount = 40000; 
-                if(perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30)
+                if(audience > 30) {
+                    thisAmount += 1000 * (audience - 30)
                 }
-                break;
+               break;
             }
             case "comedy": {
-                thisAmount = 30000; 
-                if(perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20)
+                thisAmount = 30000;
+                if(audience > 20) {
+                    thisAmount += 10000 + 500 * (audience - 20)
                 }
-                thisAmount += 300 * perf.audience
-                break;
+                return thisAmount += 300 * audience
             }
             default: {
-                throw new Error(`알 수 없는 장르 ${play.type}`)
+                throw new Error(`알 수 없는 장르 ${perf.type}`)
             }
         }
+        return thisAmount;
+    }
+
+    for(let perf of invocie.performances) {
+        const play = plays[perf.playID]
+        let thisAmount = amountFor(perf, play);
+
+      
         // 포인트를 적립한다 
         volumeCredits += Math.max(perf.audience - 30, 0);
 
