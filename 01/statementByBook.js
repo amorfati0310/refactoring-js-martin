16d9f285003032bce4a2b2a565b8eb8acd85e25e
amorfati0310 <lyhy0310@gmail.com>
2020-06-06 02:27:31 +0900

    prettier write 적용

--- 01/statementByBook.js
+++ 01/statementByBook.js
@@ -1,94 +1,92 @@
-// checkList 
+// checkList
 
-// 별도의 함수로 빼냈을 때 값을 변경시키는지 ? 
-// 매개변수로서만 참조만 한다면 괜찮음 
+// 별도의 함수로 빼냈을 때 값을 변경시키는지 ?
+// 매개변수로서만 참조만 한다면 괜찮음
 
-// 하나 고친 후 테스트 코드를 돌려서 확인한다. 
-// -> 변경점이 많은 후에 테스트 하면 -> 디버깅이 어렵기 때문이다 
+// 하나 고친 후 테스트 코드를 돌려서 확인한다.
+// -> 변경점이 많은 후에 테스트 하면 -> 디버깅이 어렵기 때문이다
 
-// 조금씩 수정하여 피드백 주기를 짧게 가져간다. 
+// 조금씩 수정하여 피드백 주기를 짧게 가져간다.
 
-// 테스트 -> 이상 없으면 커밋한다 
+// 테스트 -> 이상 없으면 커밋한다
 // -> 난 반대네 커밋하고 -> 디버깅 했는데 습관이 _ㅇ ...
 // 그리고 어느정도 피쳐개발 수준으로 커밋이 쌓이면 푸시를 한다 ->
 
-// 변수들 명확한 의미를 가져가자 
+// 변수들 명확한 의미를 가져가자
 
-// play 변수 제거 하기 
-// performance를 통해서 가져오는 값이므로 -> 굳이 매개변수로 전달할 필요가 없다 
-// amountFor 안에서 계산 해주자 
-// 긴 함수를 쪼갤 때 이런 부분을 최대한 제거해주라 
+// play 변수 제거 하기
+// performance를 통해서 가져오는 값이므로 -> 굳이 매개변수로 전달할 필요가 없다
+// amountFor 안에서 계산 해주자
+// 긴 함수를 쪼갤 때 이런 부분을 최대한 제거해주라
 // 임시변수를 질의함수로 바꾸기 ?
 
-
-// 다음 변수 인라인 하기 
-// 가장 공감이 조금 덜한 부분 
+// 다음 변수 인라인 하기
+// 가장 공감이 조금 덜한 부분
 // 인라인으로 넣어준다라 ...
-// playFor(perf) 이게 쓰는 부분이 많은 걸 play로 줄여서 
+// playFor(perf) 이게 쓰는 부분이 많은 걸 play로 줄여서
 // 여러군데 써주면 이게 더 가독성이 좋을 것 같은데 ???
 
- 
-const usd = (aNumber) => new Intl.NumberFormat("en-Us", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2
-}).format(aNumber/ 100)
+const usd = (aNumber) =>
+  new Intl.NumberFormat('en-Us', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100)
 
-function playFor(aPerformance){
-    return plays[aPerformance.playID]
+function playFor(aPerformance) {
+  return plays[aPerformance.playID]
 }
 function statment(invocie, plays) {
-    let totalAmount = 0;
-    let result = `청구 내역 (고객명: ${inovice.customer})\n`
+  let totalAmount = 0
+  let result = `청구 내역 (고객명: ${inovice.customer})\n`
 
-
-    function amountFor(aPerformance){
-        let result = 0;
-        switch(playFor(aPerformance).type){
-            case "tragedy": {
-                result = 40000; 
-                if(aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-               break;
-            }
-            case "comedy": {
-                result = 30000;
-                if(aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20)
-                }
-                result += 300 * aPerformance.audience
-                break;
-            }
-            default: {
-                throw new Error(`알 수 없는 장르 ${playFor(aPerformance).type}`)
-            }
+  function amountFor(aPerformance) {
+    let result = 0
+    switch (playFor(aPerformance).type) {
+      case 'tragedy': {
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
         }
-        return result;
-    }
-
-    let volumeCredits = 0;
-    function volumeCreditsFor(perf){
-        let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        if('comedy' === playFor(perf).type){
-            volumeCredits += Math.floor(perf.audience / 5)
+        break
+      }
+      case 'comedy': {
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
         }
-        return volumeCredits
+        result += 300 * aPerformance.audience
+        break
+      }
+      default: {
+        throw new Error(`알 수 없는 장르 ${playFor(aPerformance).type}`)
+      }
     }
+    return result
+  }
 
-    // for 문 분리
-
-    for(let perf of invocie.performances) {
-        // 청구 내역을 출력한다. 
-        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} 석) \n`
-        totalAmount += amountFor(perf)
+  let volumeCredits = 0
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0
+    volumeCredits += Math.max(perf.audience - 30, 0)
+    if ('comedy' === playFor(perf).type) {
+      volumeCredits += Math.floor(perf.audience / 5)
     }
+    return volumeCredits
+  }
 
-    for(let perf of invocie.performances) {
-        volumeCredits += volumeCreditsFor(perf)
-    }
+  // for 문 분리
 
-    result += `총액 ${usd(totalAmount)}`
-    return result;
+  for (let perf of invocie.performances) {
+    // 청구 내역을 출력한다.
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} 석) \n`
+    totalAmount += amountFor(perf)
+  }
+
+  for (let perf of invocie.performances) {
+    volumeCredits += volumeCreditsFor(perf)
+  }
+
+  result += `총액 ${usd(totalAmount)}`
+  return result
 }
--- 01/statementByDali.js
+++ 01/statementByDali.js
@@ -8,81 +8,71 @@ plays  [play]
 return result string
 */
 
-// 1. return type 이나 param type 명시되면 좋을 듯  
-// 2. statement -> getStatement 
-// 함수는 동사 + 명사 행위나 의도를 나타내면 더 좋을 듯 
-// 3. destructuring 활용 해서 프로퍼티들을 좀 나눈다 
+// 1. return type 이나 param type 명시되면 좋을 듯
+// 2. statement -> getStatement
+// 함수는 동사 + 명사 행위나 의도를 나타내면 더 좋을 듯
+// 3. destructuring 활용 해서 프로퍼티들을 좀 나눈다
 // 4. for of -> 보다 Array instance methods가 가독성 밑 조작이 편의하다.
 
+// 5. thisAmout 구하는 switchCase를 분리하자
 
-// 5. thisAmout 구하는 switchCase를 분리하자 
-
-
-//6. 행위별로 함수를 분리한다. 
+//6. 행위별로 함수를 분리한다.
 // 6.1 토탈 어마운트 계산
 // 6.2 포인트 적립
 // 6.3 청구내역 출력
 // ETC `
-// format  new Intl -> 잘 모름 일단 넘어가자 
+// format  new Intl -> 잘 모름 일단 넘어가자
 
+function statment({ performances, customer }, plays) {
+  const initStatement = `청구 내역 (고객명: ${customer})\n`
+  const format = new Intl.NumberFormat('en-Us', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format
 
-
-function statment({performances, customer}, plays) {
-    const initStatement = `청구 내역 (고객명: ${customer})\n`
-    const format = new Intl.NumberFormat("en-Us", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format
-
-
-    const getAmountByType = (type, audience) => {
-        switch(type){
-            case "tragedy": {
-                let amount = 40000; 
-                if(audience > 30) {
-                    amount += 1000 * (audience - 30)
-                }
-                return amount;
-            }
-            case "comedy": {
-                let amount = 30000;
-                if(audience > 20) {
-                    amount += 10000 + 500 * (audience - 20)
-                }
-                return amount += 300 * audience
-            }
-            default: {
-                throw new Error(`알 수 없는 장르 ${type}`)
-            }
+  const getAmountByType = (type, audience) => {
+    switch (type) {
+      case 'tragedy': {
+        let amount = 40000
+        if (audience > 30) {
+          amount += 1000 * (audience - 30)
         }
+        return amount
+      }
+      case 'comedy': {
+        let amount = 30000
+        if (audience > 20) {
+          amount += 10000 + 500 * (audience - 20)
+        }
+        return (amount += 300 * audience)
+      }
+      default: {
+        throw new Error(`알 수 없는 장르 ${type}`)
+      }
     }
+  }
 
- 
+  const toatalAmount = performances.reduce((acc, { playID, audience }) => {
+    const { type } = plays[playID]
+    const amount = getAmountByType(type, audience)
+    return amount + acc
+  }, 0)
 
+  const volumeCredits = performances.reduce((acc, { audience, playID }) => {
+    const { type } = plays[playID]
+    let credits = acc + Math.max(audience - 30, 0)
+    if (type === 'comedy') {
+      credits += Math.floor(audience / 5)
+    }
+    return credits
+  }, 0)
 
-    const toatalAmount = performances.reduce((acc, {playID, audience}) => {
-        const {type} = plays[playID]
-        const amount = getAmountByType(type, audience)
-        return amount + acc;
-    }, 0);
-
-
-    const volumeCredits = performances.reduce((acc, {audience, playID}) => {
-        const {type} = plays[playID]
-        let credits = acc + Math.max(audience - 30, 0)
-        if(type === 'comedy'){
-            credits += Math.floor(audience / 5)
-        }
-        return credits;
-    }, 0);
-
-    const printStatementsByPeformance = performances.reduce((acc, {audience, playID}) => {
-        const {type, name} = plays[playID]
-        result += `${name}: ${format(getAmountByType(type, audience) / 100)} (${audience} 석) \n`
-       
-    }, ``)
-    return `
+  const printStatementsByPeformance = performances.reduce((acc, { audience, playID }) => {
+    const { type, name } = plays[playID]
+    result += `${name}: ${format(getAmountByType(type, audience) / 100)} (${audience} 석) \n`
+  }, ``)
+  return `
         ${initStatement}
         ${printStatementsByPeformance}
         총액 ${format(totalAmount / 100)}
